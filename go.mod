91f658451c370242f473a81d3863e1e54f7f316f
Michael Helmling <michaelhelmling@posteo.de>
2020-05-31 21:38:27 +0200

    WIP add embedded grpcweb proxy

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/supermihi/karlchencloud
 go 1.14
 
 require (
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/golang/protobuf v1.4.1
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
+	github.com/improbable-eng/grpc-web v0.12.0 // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/stretchr/testify v1.5.1
 	google.golang.org/grpc v1.28.1
 	google.golang.org/protobuf v1.22.0
--- server/grpc.go
+++ server/grpc.go
@@ -3,14 +3,18 @@ package server
 import (
 	"context"
 	grpcauth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/supermihi/karlchencloud/api"
 	"github.com/supermihi/karlchencloud/doko/game"
 	"github.com/supermihi/karlchencloud/doko/match"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 )
 
@@ -26,6 +30,29 @@ func NewServer(room Room, auth Auth) *dokoserver {
 	return &dokoserver{
 		room: room, auth: auth, streams: newStreams(),
 	}
+}
+
+// https://rogchap.com/2019/07/26/in-process-grpc-web-proxy/
+func WrapServer(grpcServer *grpc.Server) *http.Server {
+	grpcWebServer := grpcweb.WrapServer(grpcServer)
+
+	httpServer := &http.Server{
+		Handler: h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.ProtoMajor == 2 {
+				grpcWebServer.ServeHTTP(w, r)
+			} else {
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-User-Agent, X-Grpc-Web")
+				w.Header().Set("grpc-status", "")
+				w.Header().Set("grpc-message", "")
+				if grpcWebServer.IsGrpcWebRequest(r) {
+					grpcWebServer.ServeHTTP(w, r)
+				}
+			}
+		}), &http2.Server{}),
+	}
+	return httpServer
 }
 func StartServer(users Users, port string) {
 	lis, err := net.Listen("tcp", port)
