936657b535e0928243bc58a2dca5c17ae2486eec
Benjamin Eikel <benjamin@eikel.org>
2021-03-26 15:40:46 +0100

    Change generate command for Go
    Taken from gRPC Go Getting Started and adapted.

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo "generating go code ..."
-protoc -I. api/karlchen.proto --go_out=plugins=grpc:api
+# see https://grpc.io/docs/languages/go/quickstart/#prerequisites
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    api/karlchen.proto
 echo "generating typescript code ..."
 protoc -I api karlchen.proto --js_out=import_style=commonjs:frontend/src/api --grpc-web_out=import_style=typescript,mode=grpcwebtext:frontend/src/api
 echo "done."
