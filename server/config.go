a247b8584011d9b92fa70dfdc2dd805ed8439beb
m8mble <m8mble@vivaldi.net>
2021-04-23 11:02:45 +0200

    Make KC_DBG_SEED a proper configuration knob

--- server/config.go
+++ server/config.go
@@ -8,6 +8,7 @@ type ServerConfig struct {
 	Room struct {
 		ConstantTableId    string `yaml:"tableId" env:"CONSTANT_TABLE_ID" env-default:""`
 		ConstantInviteCode string `yaml:"inviteCode" env:"CONSTANT_INVITE_CODE" env-default:""`
+		Seed               int64  `yaml:"seed" env:"KC_DBG_SEED" env-default:""`
 	} `yaml:"room"`
 	NoProxy bool `yaml:"noProxy" env:"NO_PROXY" env-default:"0"`
 }
