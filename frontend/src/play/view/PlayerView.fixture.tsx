3c330ee18892deb7cbdece1197cc3854d1227208
Daniel Jones <danielrjones1@gmail.com>
2021-04-23 16:18:20 +0200

    Run prettier on PlayerView.fixture.tsx

--- frontend/src/play/view/PlayerView.fixture.tsx
+++ frontend/src/play/view/PlayerView.fixture.tsx
@@ -5,15 +5,20 @@ import { User } from 'model/core';
 import * as mockPlayers from 'mocks/players';
 import PlayerView from './PlayerView';
 
-
 export default {
-  'normal': <PlayerView user={mockPlayers.face}
-                        bids={[]} turn={true} />,
-  'solo': <PlayerView user={mockPlayers.left} soloGame={GameType.DIAMONDS_SOLO}
-                      bids={[]} turn={true} />,
-  'declaration': <PlayerView user={mockPlayers.right} declaration={Declaration.gesund}
-                             bids={[]} turn={false} />,
-  'bids': <PlayerView user={mockPlayers.right}
-                      bids={[BidType.RE_BID, BidType.RE_NO_NINETY, BidType.RE_NO_SIXTY]}
-                      turn={false} />,
+  normal: <PlayerView user={mockPlayers.face} bids={[]} turn={true} />,
+  solo: (
+    <PlayerView user={mockPlayers.left} soloGame={GameType.DIAMONDS_SOLO} bids={[]} turn={true} />
+  ),
+  declaration: (
+    <PlayerView user={mockPlayers.right} declaration={Declaration.gesund} bids={[]} turn={false} />
+  ),
+
+  bids: (
+    <PlayerView
+      user={mockPlayers.right}
+      bids={[BidType.RE_BID, BidType.RE_NO_NINETY, BidType.RE_NO_SIXTY]}
+      turn={false}
+    />
+  ),
 };
